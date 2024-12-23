07ee0af3594a26d6db8ae087bc6b114388e3cb4c
NTyranowski <nikodemtyranowski@onet.pl>
2024-12-23 13:15:22 +0100

    1

---
+++ Strona do rezerwacji biletów/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Strona_do_rezerwacji_biletów.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Strona do rezerwacji biletów/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Strona_do_rezerwacji_biletów.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
