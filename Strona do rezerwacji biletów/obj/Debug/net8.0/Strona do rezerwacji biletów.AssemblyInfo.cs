543718f721587a5a33ced4832dd5b08f1c0c6876
NTyranowski <nikodemtyranowski@onet.pl>
2024-12-27 13:55:25 +0100

    g

--- Strona do rezerwacji biletów/obj/Debug/net8.0/Strona do rezerwacji biletów.AssemblyInfo.cs
+++ Strona do rezerwacji biletów/obj/Debug/net8.0/Strona do rezerwacji biletów.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Strona do rezerwacji biletów")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d7309dea4c5cb119fc16ab949658ec3ceebdd5a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+494f4c070b0def8844d3ebb78ab9ad9ce22806d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Strona do rezerwacji biletów")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Strona do rezerwacji biletów")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
